889170f95d9543d5dfe896116223c915d10b46f2
WindowsP132 <WindowsP132@Win10.com>
2020-08-10 02:00:04 +0530

    Add icon to the error status

--- resources/js/components/NotificationMessage.vue
+++ resources/js/components/NotificationMessage.vue
@@ -14,7 +14,7 @@
             <img src="https://img.icons8.com/color/32/000000/mamits.png"/>
           </span>
           <span v-else-if="notification.data.level === 'error'">
-            <svg aria-hidden="true" data-prefix="fas" data-icon="exclamation-circle" class="svg-inline--fa fa-exclamation-circle fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#c62828" d="M504 256c0 136.997-111.043 248-248 248S8 392.997 8 256C8 119.083 119.043 8 256 8s248 111.083 248 248zm-248 50c-25.405 0-46 20.595-46 46s20.595 46 46 46 46-20.595 46-46-20.595-46-46-46zm-43.673-165.346l7.418 136c.347 6.364 5.609 11.346 11.982 11.346h48.546c6.373 0 11.635-4.982 11.982-11.346l7.418-136c.375-6.874-5.098-12.654-11.982-12.654h-63.383c-6.884 0-12.356 5.78-11.981 12.654z"></path></svg>
+            <img src="https://img.icons8.com/cotton/32/000000/sms-1.png"/>
           </span>
         </span>
       <div class="table-cell w-full py-4 pl-4">
